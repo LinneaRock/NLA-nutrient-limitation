5910bffd8e8ccc30003606fee21d2edf9cecc43c
LinneaRock <lrock@wisc.edu>
2022-04-06 11:17:23 -0600

    NLA 2012 dataset

--- Code/Call_data_2012NLA.R
+++ Code/Call_data_2012NLA.R
@@ -29,7 +29,7 @@ nla2012_wq1 <- nla2012_wq |>
 # filter for useful site informaiton
 nla2012_siteinfo1 <- nla2012_siteinfo |>
   select(SITE_ID, UID, VISIT_NO, DATE_COL, SITETYPE, LON_DD83, LAT_DD83, EPA_REG, WGT_ALL, URBAN, LAKE_ORIGIN, AREA_HA, ELEVATION, HUC8) |>
-  left_join(uniques1) |>
+  left_join(uniques1) |> # join with uniques dataset
   rename(VISIT_ID = UID,
          SITE_TYPE = SITETYPE,
          LON_DD = LON_DD83,
@@ -43,5 +43,25 @@ nla2012_siteinfo1 <- nla2012_siteinfo |>
 
 # filter for condition information
 nla2012_condition1 <- nla2012_condition |>
-  select(SITE_ID, UID,AGGR_ECO9_2015, CHLA_COND, NTL_COND, PTL_COND, TROPHIC_STATE)
+  select(SITE_ID, UID, AGGR_ECO9_2015, CHLA_COND, NTL_COND, PTL_COND, TROPHIC_STATE) |>
+  rename(VISIT_ID = UID)
+
+
+nla2012 <- left_join(nla2012_wq1, nla2012_siteinfo1) |>
+  select(-NO3N_PPM)
+
+nla2012 <- left_join(nla2012, nla2012_condition1)
+
+
+#add column for molar TN:TP, TN, TP
+Pmol <- molMass("P") * 1000000 #ug/mol 
+Nmol <- molMass("N") * 1000 #mg/mol
+
+nla2012.tntp <- nla2012 |>
+  mutate(tn.tp = (NTL_PPM/Nmol)/(PTL_PPB/Pmol),
+         TN_mol = (NTL_PPM)/Nmol,
+         TP_mol = (PTL_PPB)/Pmol)
+
+
+write.csv(nla2012.tntp, "Data/NLA_2012.csv")
 
