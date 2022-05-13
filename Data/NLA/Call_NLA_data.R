4ad1e650e2e1c4d1a61792e358eba76beadaddaf
LinneaRock <lrock@wisc.edu>
2022-05-13 14:53:20 -0600

    calculated trophic states and saved new csv files.

--- Data/NLA/Call_NLA_data.R
+++ Data/NLA/Call_NLA_data.R
@@ -9,19 +9,19 @@ library(lubridate)
 
 NLA07 <- read.csv("Data/NLA/NLA_2007.csv") |>
   select(-X) |>
-  mutate(DATE_COL = as.Date(DATE_COL, format = "%m/%d/%Y"))
+  mutate(DATE_COL = as.Date(DATE_COL))
 
 #str(NLA07) # what variables need to be altered (e.g. to as date or as numeric, etc.)? 
 
 NLA12 <- read.csv("Data/NLA/NLA_2012.csv") |>
-  select(-X, -X.1) |>
-  mutate(DATE_COL = as.Date(DATE_COL, format = "%m/%d/%Y"))
+  select(-X) |>
+  mutate(DATE_COL = as.Date(DATE_COL))
 
 #str(NLA12) # what variables need to be altered (e.g. to as date or as numeric, etc.)? 
 
 NLA17 <- read.csv("Data/NLA/NLA_2017.csv") |>
   select(-X) |>
-  mutate(DATE_COL = as.Date(DATE_COL, format = "%d-%b-%y"),
+  mutate(DATE_COL = as.Date(DATE_COL),
          HUC_8 = as.integer(substring(HUC_8, 2)))
 
 #str(NLA17) # what variables need to be altered (e.g. to as date or as numeric, etc.)? 
