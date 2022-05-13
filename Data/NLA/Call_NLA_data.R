7b573d1818d00ef3036a9f3f0bf92a8750933e6d
LinneaRock <lrock@wisc.edu>
2022-05-13 13:45:02 -0600

    update to call in data

--- Data/NLA/Call_NLA_data.R
+++ Data/NLA/Call_NLA_data.R
@@ -7,19 +7,19 @@ library(lubridate)
 
 ## Looking at plots of  all data
 
-NLA07 <- read.csv("DAta/NLA_2007.csv") |>
-  select(-X, -X.1) |>
+NLA07 <- read.csv("Data/NLA/NLA_2007.csv") |>
+  select(-X) |>
   mutate(DATE_COL = as.Date(DATE_COL, format = "%m/%d/%Y"))
 
 #str(NLA07) # what variables need to be altered (e.g. to as date or as numeric, etc.)? 
 
-NLA12 <- read.csv("Data/NLA_2012.csv") |>
+NLA12 <- read.csv("Data/NLA/NLA_2012.csv") |>
   select(-X, -X.1) |>
   mutate(DATE_COL = as.Date(DATE_COL, format = "%m/%d/%Y"))
 
 #str(NLA12) # what variables need to be altered (e.g. to as date or as numeric, etc.)? 
 
-NLA17 <- read.csv("Data/NLA_2017.csv") |>
+NLA17 <- read.csv("Data/NLA/NLA_2017.csv") |>
   select(-X) |>
   mutate(DATE_COL = as.Date(DATE_COL, format = "%d-%b-%y"),
          HUC_8 = as.integer(substring(HUC_8, 2)))
