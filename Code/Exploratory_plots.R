44060e56ea1cfd4e8dd1cacc5e174e058fea8239
LinneaRock <lrock@wisc.edu>
2022-05-23 14:12:49 -0600

    created lake size figure

--- Code/Exploratory_plots.R
+++ Code/Exploratory_plots.R
@@ -2,34 +2,39 @@
 
 library(tidyverse)
 library(colorblindr)
-library(lubridate)
+
 
 ## Looking at plots of  all data
 
-NLA07 <- read.csv("DAta/NLA_2007.csv") |>
-  select(-X, -X.1) |>
-  mutate(DATE_COL = as.Date(DATE_COL, format = "%m/%d/%Y"))
+# NLA07 <- read.csv("DAta/NLA_2007.csv") |>
+#   select(-X, -X.1) |>
+#   mutate(DATE_COL = as.Date(DATE_COL, format = "%m/%d/%Y"))
+# 
+# #str(NLA07) # what variables need to be altered (e.g. to as date or as numeric, etc.)? 
+# 
+# NLA12 <- read.csv("Data/NLA_2012.csv") |>
+#   select(-X, -X.1) |>
+#   mutate(DATE_COL = as.Date(DATE_COL, format = "%m/%d/%Y"))
+# 
+# #str(NLA12) # what variables need to be altered (e.g. to as date or as numeric, etc.)? 
+# 
+# NLA17 <- read.csv("Data/NLA_2017.csv") |>
+#   select(-X) |>
+#   mutate(DATE_COL = as.Date(DATE_COL, format = "%d-%b-%y"),
+#          HUC_8 = as.integer(substring(HUC_8, 2)))
+# 
+# #str(NLA17) # what variables need to be altered (e.g. to as date or as numeric, etc.)? 
+# 
+# 
+# 
+# # combine and plot all TN:TP
+# all_NLA <- bind_rows(NLA07, NLA12, NLA17) |>
+#   mutate(year = as.character(year(DATE_COL)))
 
-#str(NLA07) # what variables need to be altered (e.g. to as date or as numeric, etc.)? 
-
-NLA12 <- read.csv("Data/NLA_2012.csv") |>
-  select(-X, -X.1) |>
-  mutate(DATE_COL = as.Date(DATE_COL, format = "%m/%d/%Y"))
-
-#str(NLA12) # what variables need to be altered (e.g. to as date or as numeric, etc.)? 
-
-NLA17 <- read.csv("Data/NLA_2017.csv") |>
-  select(-X) |>
-  mutate(DATE_COL = as.Date(DATE_COL, format = "%d-%b-%y"),
-         HUC_8 = as.integer(substring(HUC_8, 2)))
-
-#str(NLA17) # what variables need to be altered (e.g. to as date or as numeric, etc.)? 
+## instead of using above code, just call in the data
+source("Data/NLA/Call_NLA_data.R")
 
 
-
-# combine and plot all TN:TP
-all_NLA <- bind_rows(NLA07, NLA12, NLA17) |>
-  mutate(year = as.character(year(DATE_COL)))
 
 ggplot(all_NLA) +
   geom_point(aes(TP_mol, TN_mol, fill = year), size = 2.5, shape = 21, alpha = 0.8) +
