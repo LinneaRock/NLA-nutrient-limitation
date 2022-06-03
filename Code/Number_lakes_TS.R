fba6c75d6f2e7cde7bf1162aeee2133a4f0d4b17
Linnea Ann Rock <lrock1@uwyo.edu>
2022-06-03 16:02:52 -0600

    continuation

--- Code/Number_lakes_TS.R
+++ Code/Number_lakes_TS.R
@@ -1,6 +1,6 @@
 
 # Script to calculate number of lakes classified in trophic state in each ecoregion and year.
-
+source("Code/Functions/no_lakes_ts.R")
 # using spsurvey package in R -- reports number of sampled lakes along with percentage of lakes it representson the landscape and standard error of that assessment
 
 library(tidyverse)
@@ -9,6 +9,7 @@ library(spsurvey)
 ## citation("spsurvey")
 
 
+#first, subset the data
 
 ts <- all_NLA |>
   select(UNIQUE_ID, year, WGT_NLA, ECO_REG_NAME, TSTATE_CHL, TSTATE_TP, TSTATE_TN, LAT_DD, LON_DD) |>
@@ -19,35 +20,7 @@ ts <- all_NLA |>
 ################################################################################
 ###################################Chla#########################################
 
-# 2007
-chla_2007_ecoreg <- cat_analysis(ts |> filter(year == 2007), subpop = "ECO_REG_NAME", vars = "TSTATE_CHL", weight = "WGT_NLA", xcoord = "LON_DD", ycoord = "LAT_DD")
-
-chla_2007_ecoreg.1 <- chla_2007_ecoreg |>
-  select(Subpopulation, Category, nResp, Estimate.P, StdError.P) |>
-  mutate(year = 2007)
-
-# 2012
-chla_2012_ecoreg <- cat_analysis(ts |> filter(year == 2012), subpop = "ECO_REG_NAME", vars = "TSTATE_CHL", weight = "WGT_NLA", xcoord = "LON_DD", ycoord = "LAT_DD")
-
-chla_2012_ecoreg.1 <- chla_2012_ecoreg |>
-  select(Subpopulation, Category, nResp, Estimate.P, StdError.P) |>
-  mutate(year = 2012)
-
-# 2017
-chla_2017_ecoreg <- cat_analysis(ts |> filter(year == 2017), subpop = "ECO_REG_NAME", vars = "TSTATE_CHL", weight = "WGT_NLA", xcoord = "LON_DD", ycoord = "LAT_DD")
-
-chla_2017_ecoreg.1 <- chla_2017_ecoreg |>
-  select(Subpopulation, Category, nResp, Estimate.P, StdError.P) |>
-  mutate(year = 2017)
-
-chla_national <- cat_analysis(ts, vars = "TSTATE_CHL", subpop = "year", weight = "WGT_NLA", xcoord = "LON_DD", ycoord = "LAT_DD")
-
-chla_national.1 <- chla_national|>
-  select(Subpopulation, Category, nResp, Estimate.P, StdError.P) |>
-  mutate(year = Subpopulation,
-         Subpopulation = "National")
-
-chla_trophic_states <- rbind(chla_national.1, chla_2007_ecoreg.1, chla_2012_ecoreg.1, chla_2017_ecoreg.1) |>
+chla_trophic_states <- no_lakes_ts(ts, "TSTATE_CHL") |> # And do all this dumb formatting
   filter(Category != "Total") |>
   mutate(fill_column = paste(nResp, "(", round(Estimate.P, 1), "±", round(StdError.P, 1), ")")) |>
   mutate(column_names = paste(Category, ".", year)) |>
@@ -73,35 +46,8 @@ chla_trophic_states |>
 ################################################################################
 ###################################TP###########################################
 
-# 2007
-TP_2007_ecoreg <- cat_analysis(ts |> filter(year == 2007), subpop = "ECO_REG_NAME", vars = "TSTATE_TP", weight = "WGT_NLA", xcoord = "LON_DD", ycoord = "LAT_DD")
 
-TP_2007_ecoreg.1 <- TP_2007_ecoreg |>
-  select(Subpopulation, Category, nResp, Estimate.P, StdError.P) |>
-  mutate(year = 2007)
-
-# 2012
-TP_2012_ecoreg <- cat_analysis(ts |> filter(year == 2012), subpop = "ECO_REG_NAME", vars = "TSTATE_TP", weight = "WGT_NLA", xcoord = "LON_DD", ycoord = "LAT_DD")
-
-TP_2012_ecoreg.1 <- TP_2012_ecoreg |>
-  select(Subpopulation, Category, nResp, Estimate.P, StdError.P) |>
-  mutate(year = 2012)
-
-# 2017
-TP_2017_ecoreg <- cat_analysis(ts |> filter(year == 2017), subpop = "ECO_REG_NAME", vars = "TSTATE_TP", weight = "WGT_NLA", xcoord = "LON_DD", ycoord = "LAT_DD")
-
-TP_2017_ecoreg.1 <- TP_2017_ecoreg |>
-  select(Subpopulation, Category, nResp, Estimate.P, StdError.P) |>
-  mutate(year = 2017)
-
-TP_national <- cat_analysis(ts, vars = "TSTATE_TP", subpop = "year", weight = "WGT_NLA", xcoord = "LON_DD", ycoord = "LAT_DD")
-
-TP_national.1 <- TP_national|>
-  select(Subpopulation, Category, nResp, Estimate.P, StdError.P) |>
-  mutate(year = Subpopulation,
-         Subpopulation = "National")
-
-TP_trophic_states <- rbind(TP_national.1, TP_2007_ecoreg.1, TP_2012_ecoreg.1, TP_2017_ecoreg.1) |>
+TP_trophic_states <- no_lakes_ts(ts, "TSTATE_TP") |> # And do all this dumb formatting
   filter(Category != "Total") |>
   mutate(fill_column = paste(nResp, "(", round(Estimate.P, 1), "±", round(StdError.P, 1), ")")) |>
   mutate(column_names = paste(Category, ".", year)) |>
@@ -119,35 +65,7 @@ TP_trophic_states |>
 ################################################################################
 ###################################TN###########################################
 
-# 2007
-TN_2007_ecoreg <- cat_analysis(ts |> filter(year == 2007), subpop = "ECO_REG_NAME", vars = "TSTATE_TN", weight = "WGT_NLA", xcoord = "LON_DD", ycoord = "LAT_DD")
-
-TN_2007_ecoreg.1 <- TN_2007_ecoreg |>
-  select(Subpopulation, Category, nResp, Estimate.P, StdError.P) |>
-  mutate(year = 2007)
-
-# 2012
-TN_2012_ecoreg <- cat_analysis(ts |> filter(year == 2012), subpop = "ECO_REG_NAME", vars = "TSTATE_TN", weight = "WGT_NLA", xcoord = "LON_DD", ycoord = "LAT_DD")
-
-TN_2012_ecoreg.1 <- TN_2012_ecoreg |>
-  select(Subpopulation, Category, nResp, Estimate.P, StdError.P) |>
-  mutate(year = 2012)
-
-# 2017
-TN_2017_ecoreg <- cat_analysis(ts |> filter(year == 2017), subpop = "ECO_REG_NAME", vars = "TSTATE_TN", weight = "WGT_NLA", xcoord = "LON_DD", ycoord = "LAT_DD")
-
-TN_2017_ecoreg.1 <- TN_2017_ecoreg |>
-  select(Subpopulation, Category, nResp, Estimate.P, StdError.P) |>
-  mutate(year = 2017)
-
-TN_national <- cat_analysis(ts, vars = "TSTATE_TN", subpop = "year", weight = "WGT_NLA", xcoord = "LON_DD", ycoord = "LAT_DD")
-
-TN_national.1 <- TN_national|>
-  select(Subpopulation, Category, nResp, Estimate.P, StdError.P) |>
-  mutate(year = Subpopulation,
-         Subpopulation = "National")
-
-TN_trophic_states <- rbind(TN_national.1, TN_2007_ecoreg.1, TN_2012_ecoreg.1, TN_2017_ecoreg.1) |>
+TN_trophic_states <- no_lakes_ts(ts, "TSTATE_TN") |> # And do all this dumb formatting
   filter(Category != "Total") |>
   mutate(fill_column = paste(nResp, "(", round(Estimate.P, 1), "±", round(StdError.P, 1), ")")) |>
   mutate(column_names = paste(Category, ".", year)) |>
