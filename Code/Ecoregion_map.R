2f8bfb5634364238d39c80a67c841da776bec2c0
Linnea Ann Rock <lrock1@uwyo.edu>
2022-07-07 12:00:12 -0600

    map!!

--- Code/Ecoregion_map.R
+++ Code/Ecoregion_map.R
@@ -1,5 +1,17 @@
 #### Aggregated ecoregion map ####
 
 library(sf)
+library(tidyverse)
+library(colorblindr)
+source("C:/Users/lrock1/Downloads/PhD_code/STOICH_NARSchallenge/Data/NLA/Call_NLA_data.R")
 
-sf <- read_sf("C:/Users/lrock1/Downloads/gagesII_9322_point_shapefile/gagesII_9322_sept30_2011.shp")
+regions.sf <- read_sf("Data/aggr_ecoregions_2015/Aggr_Ecoregions_2015.shp")
+nla_locations.sf <- st_as_sf(all_NLA, coords = c("LON_DD", "LAT_DD"), crs = 4326)
+
+ggplot() +
+  geom_sf(data = regions.sf, aes(fill = WSA9_NAME), alpha = 0.25) +
+  geom_sf(data = nla_locations.sf |> filter(year == 2007, !is.na(TSTATE_CHL)), aes(color = TSTATE_CHL), size = 2) +
+  theme_minimal() +
+  scale_fill_manual("", values = c(palette_OkabeIto_black[], '#FFFFFF')) +
+  scale_color_manual("", values = c(palette_OkabeIto[2], palette_OkabeIto[4], palette_OkabeIto[3], palette_OkabeIto[1]),
+                     labels = c("Olig.", "Meso.", "Eutro.", "Hyper.")) 
