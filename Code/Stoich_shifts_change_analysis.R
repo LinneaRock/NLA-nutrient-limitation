d50c598ba9a2e58d5592a05b0477154cfca84228
LinneaRock <lrock@wisc.edu>
2022-08-18 08:44:53 -0600

    looking at stoichiometric change analyses

--- Code/Stoich_shifts_change_analysis.R
+++ Code/Stoich_shifts_change_analysis.R
@@ -40,9 +40,9 @@ change_dat <- limits|> # total 3066 lakes for this analysis
   filter(VISIT_NO == 1) |># for this analysis, we are using just the first visit from each lake
   filter(WGT_NLA > 0) # the sp survey package is not designed to use the reference lakes, so those are ignored when using this package for analyses.
 
+###########################################################################
 
-
-### write a function for this change analysis 
+#### write a function for this change analysis ####
 stoich_change_fun <- function(data, name, year1, year2) {
   
   change_ecoreg<- change_analysis(data |> filter(year %in% c(year1, year2),
@@ -103,11 +103,268 @@ for(name in list) {
     distinct()
   
 }
+
 #check warnings and actions
 actions1217 <- data.frame(warn_df$warning, warn_df$action) # repeat sites covariance was not included in standard error, covariance estimate
 
 stoich_change1217 <- change1217 |>
   mutate(year.shift = "2012-2017")
 
-changes.final <- rbind(lim_change0712, lim_change1217) |>
+changes.final <- rbind(stoich_change0712, stoich_change1217) #|>
+  #mutate(Trophic.State = factor(Trophic.State, levels = c("Oligo.", "Meso.", "Eutro.", "Hyper.")))
+
+
+###########################################################################
+
+#### write a function for this change analysis now with trophic state ####
+stoich_change_fun <- function(data, name, year1, year2) {
+  
+  change_ecoreg<- change_analysis(data |> filter(year %in% c(year1, year2),
+                                                 ECO_REG_NAME == name), siteID = "UNIQUE_ID", subpop = "TROPHIC_STATE", vars_cont = "tn.tp", surveyID = "year", weight = "WGT_NLA", xcoord = "LON_DD", ycoord = "LAT_DD")
+  
+  change_ecoreg.1 <- change_ecoreg[["contsum_mean"]]  |>
+    dplyr::select(Subpopulation, Indicator, DiffEst, StdError) |>
+    mutate(ECO_REG = name) |>
+  rename(Trophic.State = Subpopulation)
+  
+  
+  change_national <- change_analysis(data |> filter(year %in% c(year1, year2)), siteID = "UNIQUE_ID", subpop = "TROPHIC_STATE", vars_cont = "tn.tp", surveyID = "year", weight = "WGT_NLA", xcoord = "LON_DD", ycoord = "LAT_DD")
+  
+  
+  change_national.1 <- change_national[["contsum_mean"]] |>
+    dplyr::select(Subpopulation, Indicator, DiffEst, StdError) |>
+    mutate(ECO_REG = "National") |>
+   rename(Trophic.State = Subpopulation) 
+  
+  final <- bind_rows(change_national.1, change_ecoreg.1)
+  
+}
+
+
+#### Run change analysis ####
+
+list <- as.vector(change_dat |> select(ECO_REG_NAME) |> distinct())[["ECO_REG_NAME"]]
+
+
+### change in nutrient limitations from 2007 to 2012
+change0712ts <- data.frame()
+
+for(name in list) {
+  
+  tmp <- stoich_change_fun(change_dat, name, "2007", "2012")
+  
+  change0712ts <- bind_rows(change0712ts, tmp) |> 
+    distinct()
+  
+}
+
+#check warnings and actions
+actions0712ts <- data.frame(warn_df$warning, warn_df$action) # repeat sites covariance was not included in standard error, covariance estimate
+
+stoich_change0712ts <- change0712ts |>
+  mutate(year.shift = "2007-2012")
+
+
+
+### change in nutrient limitations from 2012 to 2017
+change1217ts <- data.frame()
+
+for(name in list) {
+  
+  tmp <- stoich_change_fun(change_dat, name, "2012", "2017")
+  
+  change1217ts <- bind_rows(change1217ts, tmp) |> 
+    distinct()
+  
+}
+
+#check warnings and actions
+actions1217ts <- data.frame(warn_df$warning, warn_df$action) # repeat sites covariance was not included in standard error, covariance estimate
+
+stoich_change1217ts <- change1217ts |>
+  mutate(year.shift = "2012-2017")
+
+changes.finalts <- rbind(stoich_change0712ts, stoich_change1217ts) |>
   mutate(Trophic.State = factor(Trophic.State, levels = c("Oligo.", "Meso.", "Eutro.", "Hyper.")))
+
+###########################################################################
+
+
+#### write a function for this change analysis now with limitation ####
+stoich_change_fun <- function(data, name, year1, year2) {
+  
+  change_ecoreg<- change_analysis(data |> filter(year %in% c(year1, year2),
+                                                 ECO_REG_NAME == name), siteID = "UNIQUE_ID", subpop = "limitation", vars_cont = "tn.tp", surveyID = "year", weight = "WGT_NLA", xcoord = "LON_DD", ycoord = "LAT_DD")
+  
+  change_ecoreg.1 <- change_ecoreg[["contsum_mean"]]  |>
+    dplyr::select(Subpopulation, Indicator, DiffEst, StdError) |>
+    mutate(ECO_REG = name) |>
+    rename(Limitation = Subpopulation)
+  
+  
+  change_national <- change_analysis(data |> filter(year %in% c(year1, year2)), siteID = "UNIQUE_ID", subpop = "limitation", vars_cont = "tn.tp", surveyID = "year", weight = "WGT_NLA", xcoord = "LON_DD", ycoord = "LAT_DD")
+  
+  
+  change_national.1 <- change_national[["contsum_mean"]] |>
+    dplyr::select(Subpopulation, Indicator, DiffEst, StdError) |>
+    mutate(ECO_REG = "National") |>
+    rename(Limitation = Subpopulation) 
+  
+  final <- bind_rows(change_national.1, change_ecoreg.1)
+  
+}
+
+
+#### Run change analysis ####
+
+list <- as.vector(change_dat |> select(ECO_REG_NAME) |> distinct())[["ECO_REG_NAME"]]
+
+
+### change in nutrient limitations from 2007 to 2012
+change0712lim <- data.frame()
+
+for(name in list) {
+  
+  tmp <- stoich_change_fun(change_dat, name, "2007", "2012")
+  
+  change0712lim <- bind_rows(change0712lim, tmp) |> 
+    distinct()
+  
+}
+
+#check warnings and actions
+actions0712lim <- data.frame(warn_df$warning, warn_df$action) # repeat sites covariance was not included in standard error, covariance estimate
+
+stoich_change0712lim <- change0712lim |>
+  mutate(year.shift = "2007-2012")
+
+
+
+### change in nutrient limitations from 2012 to 2017
+change1217lim <- data.frame()
+
+for(name in list) {
+  
+  tmp <- stoich_change_fun(change_dat, name, "2012", "2017")
+  
+  change1217lim <- bind_rows(change1217lim, tmp) |> 
+    distinct()
+  
+}
+#check warnings and actions
+actions1217lim <- data.frame(warn_df$warning, warn_df$action) # repeat sites covariance was not included in standard error, covariance estimate
+
+stoich_change1217lim <- change1217lim |>
+  mutate(year.shift = "2012-2017")
+
+changes.finallim <- rbind(stoich_change0712lim, stoich_change1217lim) 
+
+
+###########################################################################
+
+#### create some figures to visualize this analysis ####
+# National plot for overall shifts
+ggplot(changes.final |>
+         filter(ECO_REG == "National")) +
+  geom_point(aes(year.shift, DiffEst, fill = year.shift), color = "black", pch = 21, size = 1, position=position_dodge(width=0.5)) +
+  geom_errorbar(aes(year.shift, DiffEst, ymin = DiffEst-StdError, ymax = DiffEst+StdError, color = year.shift), width = 0.2, position=position_dodge(width=0.5))  + 
+  theme_minimal() +
+  facet_grid(.~year.shift, scales = "free_x") +
+  #theme(strip.background = element_rect(color = "black", fill = c(palette_OkabeIto[2], palette_OkabeIto[4], palette_OkabeIto[3], palette_OkabeIto[1]), size = 1.5, linetype = "solid")) +
+  scale_fill_manual("", values = c("red4", "#336a98")) +
+  scale_color_manual("", values = c("red4", "#336a98")) +
+  theme(axis.text.x = element_blank()) +
+  geom_hline(yintercept = 0) +
+  labs(x = "",
+       y = "% Difference in N:P stoichiometry",
+       title = "National") 
+ggsave("Figures/Qtrend.Figs/overall_stoich_shift_national.png", height = 4.5, width = 6.5, units = "in", dpi = 500) 
+
+# ecoregion plots for overall shift
+ggplot(changes.final |>
+         filter(ECO_REG != "National")) +
+  geom_point(aes(year.shift, DiffEst, fill = year.shift), color = "black", pch = 21, size = 1, position=position_dodge(width=0.5)) +
+  geom_errorbar(aes(year.shift, DiffEst, ymin = DiffEst-StdError, ymax = DiffEst+StdError, color = year.shift), width = 0.2, position=position_dodge(width=0.5))  + 
+  theme_bw() +
+  facet_grid(~year.shift, scales = "free_x") +
+  facet_wrap(~ECO_REG, ncol = 3) +
+  #theme(strip.background = element_rect(color = "black", fill = c(palette_OkabeIto[2], palette_OkabeIto[4], palette_OkabeIto[3], palette_OkabeIto[1]), size = 1.5, linetype = "solid")) +
+  scale_fill_manual("", values = c("red4", "#336a98")) +
+  scale_color_manual("", values = c("red4", "#336a98")) +
+  geom_hline(yintercept = 0) +
+  labs(x = "",
+       y = "% Difference in N:P stoichiometry")
+ggsave("Figures/Qtrend.Figs/overall_stoich_shift_regional.png", height = 4.5, width = 6.5, units = "in", dpi = 500) 
+
+
+#*******************************************************************# 
+
+ggplot(changes.finalts |>
+         filter(ECO_REG == "National")) +
+  geom_point(aes(Trophic.State, DiffEst, fill = year.shift), color = "black", pch = 21, size = 1, position=position_dodge(width=0.5)) +
+  geom_errorbar(aes(Trophic.State, DiffEst, ymin = DiffEst-StdError, ymax = DiffEst+StdError, color = year.shift), width = 0.2, position=position_dodge(width=0.5))  + 
+  theme_bw() +
+  facet_grid(.~Trophic.State, scales = "free_x") +
+  #theme(strip.background = element_rect(color = "black", fill = c(palette_OkabeIto[2], palette_OkabeIto[4], palette_OkabeIto[3], palette_OkabeIto[1]), size = 1.5, linetype = "solid")) +
+  scale_fill_manual("", values = c("red4", "#336a98")) +
+  scale_color_manual("", values = c("red4", "#336a98")) +
+  theme(axis.text.x = element_blank()) +
+  geom_hline(yintercept = 0) +
+  labs(x = "",
+       y = "% Difference in N:P stoichiometry",
+       title = "National") 
+ggsave("Figures/Qtrend.Figs/ts_stoich_shift_national.png", height = 4.5, width = 6.5, units = "in", dpi = 500) 
+
+# ecoregion plots 
+ggplot(changes.finalts |>
+         filter(ECO_REG != "National")) +
+  geom_point(aes(Trophic.State, DiffEst, fill = year.shift), color = "black", pch = 21, size = 1, position=position_dodge(width=0.5)) +
+  geom_errorbar(aes(Trophic.State, DiffEst, ymin = DiffEst-StdError, ymax = DiffEst+StdError, color = year.shift), width = 0.2, position=position_dodge(width=0.5))  + 
+  theme_bw() +
+  facet_grid(~Trophic.State, scales = "free_y") +
+  facet_wrap(~ECO_REG, ncol = 3) +
+  #theme(strip.background = element_rect(color = "black", fill = c(palette_OkabeIto[2], palette_OkabeIto[4], palette_OkabeIto[3], palette_OkabeIto[1]), size = 1.5, linetype = "solid")) +
+  scale_fill_manual("", values = c("red4", "#336a98")) +
+  scale_color_manual("", values = c("red4", "#336a98")) +
+  geom_hline(yintercept = 0) +
+  labs(x = "",
+       y = "% Difference in N:P stoichiometry") +
+  scale_x_discrete(labels = c("Olig.", "Meso.", "Eutro.", "Hyper."))
+ggsave("Figures/Qtrend.Figs/ts_stoich_shift_regional.png", height = 4.5, width = 6.5, units = "in", dpi = 500) 
+
+
+
+#*******************************************************************# 
+
+ggplot(changes.finallim |>
+         filter(ECO_REG == "National")) +
+  geom_point(aes(Limitation, DiffEst, fill = year.shift), color = "black", pch = 21, size = 1, position=position_dodge(width=0.5)) +
+  geom_errorbar(aes(Limitation, DiffEst, ymin = DiffEst-StdError, ymax = DiffEst+StdError, color = year.shift), width = 0.2, position=position_dodge(width=0.5))  + 
+  theme_bw() +
+  facet_grid(.~Limitation, scales = "free_x") +
+  #theme(strip.background = element_rect(color = "black", fill = c(palette_OkabeIto[2], palette_OkabeIto[4], palette_OkabeIto[3], palette_OkabeIto[1]), size = 1.5, linetype = "solid")) +
+  scale_fill_manual("", values = c("red4", "#336a98")) +
+  scale_color_manual("", values = c("red4", "#336a98")) +
+  theme(axis.text.x = element_blank()) +
+  geom_hline(yintercept = 0) +
+  labs(x = "",
+       y = "% Difference in N:P stoichiometry",
+       title = "National") 
+ggsave("Figures/Qtrend.Figs/lim_stoich_shift_national.png", height = 4.5, width = 6.5, units = "in", dpi = 500) 
+
+# ecoregion plots 
+ggplot(changes.finallim |>
+         filter(ECO_REG != "National")) +
+  geom_point(aes(Limitation, DiffEst, fill = year.shift), color = "black", pch = 21, size = 1, position=position_dodge(width=0.5)) +
+  geom_errorbar(aes(Limitation, DiffEst, ymin = DiffEst-StdError, ymax = DiffEst+StdError, color = year.shift), width = 0.2, position=position_dodge(width=0.5))  + 
+  theme_bw() +
+  facet_grid(~Limitation, scales = "free_y") +
+  facet_wrap(~ECO_REG, ncol = 3) +
+  #theme(strip.background = element_rect(color = "black", fill = c(palette_OkabeIto[2], palette_OkabeIto[4], palette_OkabeIto[3], palette_OkabeIto[1]), size = 1.5, linetype = "solid")) +
+  scale_fill_manual("", values = c("red4", "#336a98")) +
+  scale_color_manual("", values = c("red4", "#336a98")) +
+  geom_hline(yintercept = 0) +
+  labs(x = "",
+       y = "% Difference in N:P stoichiometry") +
+  theme(axis.text.x = element_text(angle = 45))
+ggsave("Figures/Qtrend.Figs/lim_stoich_shift_regional.png", height = 4.5, width = 6.5, units = "in", dpi = 500) 
