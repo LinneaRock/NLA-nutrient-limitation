af5049729532d705507071a09a6672b56e532917
Linnea Ann Rock <lrock1@uwyo.edu>
2023-02-02 11:00:02 -0700

    figs for presentation

--- Figures/BW_presentationfigs/DarkThemeFigs.R
+++ Figures/BW_presentationfigs/DarkThemeFigs.R
@@ -59,8 +59,9 @@ ggsave("Figures/BW_presentationfigs/lim_calc.png", height = 4.5, width = 6.5, un
 
 
 ggplot(lim_changes_fullset |>
-                        filter(Subpopulation != "National")) +
-  geom_point(aes(Category,DiffEst.P, fill = Category), color = "black", pch = 21, size = 1, 
+                        filter(Subpopulation != "National",
+                               sample_set == 'All surveyed lakes')) +
+  geom_point(aes(Category,DiffEst.P, fill = Category), color = "white", pch = 21, size = 1, 
              position=position_dodge(width=0.5))+
   geom_errorbar(aes(Category, DiffEst.P, ymin = DiffEst.P-StdError.P, 
                     ymax = DiffEst.P+StdError.P, color = Category, linetype = sample_set), width = 0.2)  + 
@@ -78,14 +79,19 @@ ggplot(lim_changes_fullset |>
     plot.caption.position = "plot",
     plot.caption = element_text(hjust = 0, family = "serif"),
     legend.position = 'none')
+ggsave("Figures/BW_presentationfigs/lim_shifts_ecoreg.png", height = 6, width = 6.5, units = "in", dpi = 1200) 
 
-ggsave("Figures/BW_presentationfigs/lim_shifts_ecoreg.png", height = 4, width = 6.5, units = "in", dpi = 1200) 
+
+
+
+
 
 #national plot
 ggplot(lim_changes_fullset |>
-                     filter(Subpopulation == "National")) +
+                     filter(Subpopulation == "National",
+                            sample_set == 'All surveyed lakes')) +
   geom_point(aes(Category,DiffEst.P, fill = Category), 
-             color = "black", pch = 21, size = 1, position=position_dodge(width=0.5)) +
+             color = "white", pch = 21, size = 1, position=position_dodge(width=0.5)) +
   geom_errorbar(aes(Category, DiffEst.P, ymin = DiffEst.P-StdError.P,
                     ymax = DiffEst.P+StdError.P, color = Category, linetype = sample_set), width = 0.2)  + 
   dark_theme_bw() +
@@ -98,15 +104,49 @@ ggplot(lim_changes_fullset |>
   scale_color_manual("",values = c("grey60","red4", "#336a98"))  +
   scale_fill_manual("",values = c("grey60","red4", "#336a98")) +
   theme(legend.title = element_blank())
-
-# layout <- "
-# AAAA
-# BBBB
-# BBBB
-# "
-
-# nat_plot/ecoreg_plot +
-#   plot_layout(guides = "collect",
-#               design = layout) +
-#   plot_annotation(tag_levels = 'a', tag_suffix = ')') 
 ggsave("Figures/BW_presentationfigs/lim_shifts_national.png", height = 4, width = 6.5, units = "in", dpi = 1200) 
+
+
+
+
+
+ggplot(percent_lim1, aes(year, Estimate.P, fill = Category)) +
+  geom_bar(stat = "identity") +
+  geom_text(text_percents, mapping = aes(label = perc), position = position_stack(vjust = 0.5), size = 3) +
+  facet_wrap(~Subpopulation) +
+  scale_fill_manual("", values = c("grey60","red4", "#336a98")) +
+  dark_theme_bw() +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        strip.text.x = element_text(size = 6.5)) +
+  labs(x = "", y = "% lakes") +
+  theme(plot.caption.position = "plot",
+        plot.caption = element_text(hjust = 0, family = "serif"),
+        legend.position = 'none')
+ggsave("Figures/BW_presentationfigs/national_limbar.png", height = 4, width = 6.5, units = "in", dpi = 1200) 
+
+
+
+
+ggplot(TS_changes_fullset |> filter(sample_set == 'All surveyed lakes')) +
+  geom_point(aes(Category,DiffEst.P, fill = Category), 
+             color = "white", pch = 21, size = 1, position=position_dodge(width=0.5)) +
+  geom_errorbar(aes(Category, DiffEst.P, ymin = DiffEst.P-StdError.P, 
+                    ymax = DiffEst.P+StdError.P, color = Category, linetype = sample_set), width = 0.2)  + 
+  dark_theme_bw() +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) +
+  facet_wrap(~Subpopulation, scales = "free_y", ncol = 2) +
+  geom_hline(yintercept = 0) +
+  labs(x = "",
+       y = "% change 2007-2017")+                                                 
+  scale_color_manual("", values = c(palette_OkabeIto[2], palette_OkabeIto[4], palette_OkabeIto[3], palette_OkabeIto[1]))  +
+  scale_fill_manual("", values = c(palette_OkabeIto[2], palette_OkabeIto[4], palette_OkabeIto[3], palette_OkabeIto[1])) +
+  theme(axis.text.x = element_text(angle = 49, vjust = 1, hjust =1),
+        strip.text.x = element_text(size = 7.5),
+        plot.caption.position = "plot",
+        plot.caption = element_text(hjust = 0, family = "serif"),
+        legend.title = element_blank())
+ggsave("Figures/BW_presentationfigs/trophicstate.png", height = 4.5, width = 6.5, units = "in", dpi = 1200) 
+
+
