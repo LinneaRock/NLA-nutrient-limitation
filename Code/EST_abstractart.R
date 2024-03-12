f4d9db47c234e7878c8aec576e2a400b32974ed7
LinneaRock <lrock@wisc.edu>
2024-03-12 16:09:17 -0600

    updating abstract art for handsome

--- Code/EST_abstractart.R
+++ Code/EST_abstractart.R
@@ -14,11 +14,12 @@ muted <- c("#CC6677", "#332288", "#DDCC77", "#117733", "#88CCEE", "#882255", "#4
 
 ggplot() +
   geom_sf(data = regions.sf, aes(fill = WSA9_NAME), alpha = 0.15) +
-  geom_sf(data = nla_locations.sf |> filter(year == '2017', limitation %in% c("N-limitation","P-limitation")), aes(color = limitation), size = 0.25) +
-  geom_sf(data = nla_locations.sf |> filter(year == '2017', limitation %in% c("Co-limitation")), aes(color = limitation), size = 0.25) +
+  geom_sf(data = nla_locations.sf |> filter(year == '2017', limitation %in% c("N-limitation","P-limitation")), aes(color = limitation, shape=limitation), size = 0.5) +
+  geom_sf(data = nla_locations.sf |> filter(year == '2017', limitation %in% c("Co-limitation")), aes(color = limitation),shape=16, size = 0.5) +
   theme_minimal() +
   scale_fill_manual("", values = muted) +
   scale_color_manual("", values = c("grey60","red4", "#336a98"), labels = c('Co-limitation', 'N-limitation', 'P-limitation'))  +
+  scale_shape_manual('',values = c(15,18)) +
   guides(fill = 'none') +
   theme(legend.position = c(1, 0.4), 
         legend.key.size = unit(0.05, 'cm'), #change legend key size
@@ -26,7 +27,8 @@ ggplot() +
         legend.key.width = unit(0.05, 'cm'), #change legend key width
         legend.text = element_text(size=6), #change legend text font size
         axis.text = element_text(size = 6),
-        legend.title = element_blank())
+        legend.title = element_blank()) +
+  guides(shape='none')
 
 ggsave("Figures/AbstractArt_map.png", height = 1.75, width = 3.25, units = "in", dpi = 500)
 
